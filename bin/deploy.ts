9972c66e355d716cff5e8a20f401b37d35055703
Vladislav <vidga471@gmail.com>
2024-02-02 21:42:49 +0300

    add deploy.ts

---
+++ bin/deploy.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { OptionValues } from 'commander';
+import {
+  validateInput,
+  transformInputValue,
+  updateSettings,
+} from './dataProcessing';
+import { showWarningMessage } from './displayMessages';
+import { defaultSettings } from './constants';
+import { publish } from './publish';
+
+export const deployAction = (args: OptionValues) => {
+  try {
+    let settings: OptionValues = { ...defaultSettings, ...args };
+
+    if (settings.user) {
+      const userIndex = 1;
+      if (validateInput(settings.user as string, userIndex)) {
+        const transformedUserValue = transformInputValue(
+          settings.user as string,
+          userIndex,
+        );
+        settings = updateSettings(transformedUserValue, userIndex, settings);
+      } else {
+        showWarningMessage(
+          'The user data format is incorrect!\nDeployment has been halted!',
+        );
+        return;
+      }
+    }
+
+    publish(settings);
+  } catch (error) {
+    console.error((error as Error).message);
+  }
+};
