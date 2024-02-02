bab21ef6ff4dab08f6468a6641cab883f965f9f8
Vladislav <vidga471@gmail.com>
2024-02-02 17:34:01 +0300

    add publish.ts

---
+++ bin/publish.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { OptionValues } from 'commander';
+import ghpages from 'gh-pages';
+import { showSuccessMessage, showWarningMessage } from './displayMessages';
+
+export const publish = (options: OptionValues) => {
+  ghpages.publish(options.dir, options, (err) => {
+    if (err) {
+      showWarningMessage('Something has gone wrong!');
+      throw err;
+    }
+
+    showSuccessMessage('Deployment successfully completed!');
+  });
+};
